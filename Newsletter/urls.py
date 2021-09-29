4f2087206bc3616d68296932cb3dbf6b46ac6f41
Packman700 <53169727+Packman700@users.noreply.github.com>
2021-09-29 22:41:04 +0200

    pain (need to refactor)

--- Newsletter/urls.py
+++ Newsletter/urls.py
@@ -1,8 +1,13 @@
+from django.conf.urls import url
 from django.urls import path
+from django.conf.urls import url
 from .views import JoinNewsletter
 
 app_name = "newsletter"
 urlpatterns = [
-    path("join-newsletter/", JoinNewsletter.as_view(), name='join-newsletter'),
+    # path("join-newsletter/", JoinNewsletter.as_view(), name='join-newsletter'),
     path("join-newsletter/thanks/", JoinNewsletter.as_view(), name='join-newsletter-success'),
+    path("join-newsletter/", JoinNewsletter.as_view(), name='join-newsletter'),
+    # url(r'^join-newsletter/(?P<success>\w+)$', JoinNewsletter.as_view(), name="join-newsletter"),
+
 ]
--- Newsletter/views.py
+++ Newsletter/views.py
@@ -1,24 +1,46 @@
 from django.views.generic import FormView, TemplateView
+from django.views.generic.base import View
+from django.shortcuts import render
+
 from .form import JoinNewsletterForm
 from django.urls import reverse, reverse_lazy
 
-class JoinNewsletter(FormView):
+class JoinNewsletter(View):
     template_name = "join_newsletter.html"
+    success_template_name = "join_newsletter_success.html"
     form_class = JoinNewsletterForm
+    context = {}
     success_url = reverse_lazy('newsletter:join-newsletter-success')
-    # user_email = None
+    user_email = None
+    is_bound = True
 
-    # def post(self):
-    #     pass
+    def get(self, request, *args, **kwargs):
+        return render(request, self.get_template_names(), self.get_context_data())
+
+    def post(self, request):
+        if self.get_context_data()['form'].is_valid(self):
+            self.form_valid(self.get_context_data())
+        return render(request, self.get_template_names(), self.get_context_data())
+
+    def get_context_data(self, *args, **kwargs):
+        self.context['form'] = self.form_class
+        self.context['email'] = self.user_email if self.user_email else None
+
+        return self.context
+
+    def get_template_names(self):
+        print(self.user_email)
+        if self.user_email:
+            return self.success_template_name
+        return self.template_name
 
     def form_valid(self, form):
         form.send_confirm_mail()
-        # self.user_email = form.cleaned_data['email']
+        self.user_email = form.cleaned_data['email']
         return super().form_valid(form)
 
-    # def get_success_url(self):
-    #     pass
 
 class JoinNewsletterSuccess(TemplateView):
+    user_email = None
     pass
 # Create your views here.
