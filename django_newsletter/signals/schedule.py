c1aed73ad156abfcaa543498d92d34c447ac51f2
Packman <DominikWlodarczyk@poczta.onet.pl>
2021-11-15 15:54:12 +0100

    Prevent auto run cron schedule on create (first run in nearest cron time)

---
+++ django_newsletter/signals/schedule.py
@@ -0,0 +1,15 @@
+from django.dispatch import receiver
+from django_q.tasks import Schedule
+from django.db.models.signals import pre_save
+from croniter import croniter
+from datetime import datetime
+
+
+@receiver(pre_save, sender=Schedule)
+def modify_cron_next_run_time(sender, instance, **kwargs):
+    if instance.schedule_type == "C" and instance.cron:
+        now = datetime.now()
+        cron_schedule = instance.cron
+        cron = croniter(cron_schedule, now)
+        next_run = cron.get_next(datetime)
+        instance.next_run = next_run
