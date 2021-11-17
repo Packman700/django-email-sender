790dbf5957db891c97222241b6895377f6d9eebf
Packman <DominikWlodarczyk@poczta.onet.pl>
2021-11-17 13:40:31 +0100

    Delete wake up and use third package api to ping in every hour

--- django_newsletter/schedules/__init__.py
+++ django_newsletter/schedules/__init__.py
@@ -1,9 +1,7 @@
 from .member import (schedule_delete_not_confirmed_members,
                      schedule_delete_confirmed_members)
-from .production import schedule_event_preventing_heroku_sleep
 
 
 def init_schedules():
     schedule_delete_confirmed_members()
     schedule_delete_confirmed_members()
-    schedule_event_preventing_heroku_sleep()
